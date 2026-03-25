ad4ee5476016a42e157a7a254818544fb0a8da67
Caleb Norton <n0603919@outlook.com>
2026-03-25 14:47:05 -0500

    feat: champsim run as artifact

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
       packages = forAllSystems (
         pkgs:
         let
+          inherit (pkgs.stdenv.hostPlatform) system;
           champsim = pkgs.callPackage ./package.nix {
             champsimSettings = defaultChampsimSettings;
             # tests are kinda slow so skip
@@ -58,17 +59,45 @@
           # works beacuse each of the champsim-N binaries has a unique name so they won't conflict with each other
           # note that it can't be called "champsim-cores" since then it would try to include itself
           # (without rewriting the predicate of course)
-          champsim_cores = pkgs.buildEnv {
-            name = "champsim_cores";
-            paths = lib.attrValues (
-              lib.filterAttrs (
-                name: _: lib.hasPrefix "champsim-" name
-              ) self.packages.${pkgs.stdenv.hostPlatform.system}
-            );
-            # don't merge the share dirs since each package has its own config which will conflict
-            pathsToLink = [ "/bin" ];
-          };
-        }
+          champsim_cores =
+            pkgs.buildEnv {
+              name = "champsim_cores";
+              paths = lib.attrValues (
+                lib.filterAttrs (
+                  name: _: lib.hasPrefix "champsim-" name
+                ) self.packages.${system}
+              );
+              # don't merge the share dirs since each package has its own config which will conflict
+              pathsToLink = [ "/bin" ];
+            };
+            # runnable simulation
+        } 
+        // (
+              let
+                perlbench = pkgs.fetchurl {
+                  url = "https://dpc3.compas.cs.stonybrook.edu/champsim-traces/speccpu/400.perlbench-41B.champsimtrace.xz";
+                  hash = "sha256-GvLlsJOSDi/BUjcsBVNlgTCTHozPOtoEF13ofkCK8JE=";
+                };
+                script = name: champsim: {warmup, simulation}: pkgs.writeShellScript name ''
+                  ${lib.getExe champsim} -w${lib.toString warmup} -i${lib.toString simulation} -- ${perlbench}
+                '';
+                artifact = name: champsim: {warmup, simulation}: pkgs.runCommand name { } ''
+                  ${lib.getExe champsim} -w${lib.toString warmup} -i${lib.toString simulation} --json=$out -- ${perlbench}
+                '';
+              in
+              {
+                # make a script that runs champsim
+                perlbench-script = script "perlbench-script" champsim {
+                  warmup = 50000000;
+                  simulation = 50000000;
+                };
+                # run a script that runs champsim and produces a json output
+                perlbench-artifact = artifact "perlbench-artifact" champsim {
+                  warmup = 50000000;
+                  simulation = 50000000;
+                };
+              }
+            )
       );
 
       formatter = forAllSystems (pkgs: pkgs.nixfmt-tree);
